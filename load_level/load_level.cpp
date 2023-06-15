45181777e3dd1a4a42f4424c21cbfffded9a4d98
Beta-Cygni-A <theinnovativeadventurer@gmail.com>
2023-06-15 06:09:15 -0700

    forgot this one

--- load_level/load_level.cpp
+++ load_level/load_level.cpp
@@ -3,12 +3,12 @@
 #include <string_view>
 #include "level.h"
 
-char loadlevel(int a, int b, int c, std::string_view d)
+loadlevel(int a, int b, int c, std::string_view d)
 {
     int y_cord {a}; //states what y_row the wanted char is in
     int x_cord {b}; //states what x_row the wanted char is in
     int temp_level_length {c}; //states how long the level is
-    char text {' '}; //text to be returned
+    char text {' '};
 
     std::ifstream level_art_read{d}; //read art file
 
@@ -20,5 +20,5 @@ char loadlevel(int a, int b, int c, std::string_view d)
         text = temp_art_store[x_cord]; //give text the value of the right char from the line
     }
 
-    return text; //returning char
+    grid[y_cord][x_cord] = text;
 }
