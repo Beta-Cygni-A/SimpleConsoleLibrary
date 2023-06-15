2bf98c974de01f33695e3da00c0a0e760339e3a3
Beta-Cygni-A <theinnovativeadventurer@gmail.com>
2023-06-15 06:07:32 -0700

    moving old code, made new example x_y_code

--- gridsystem/main.cpp
+++ gridsystem/main.cpp
@@ -5,7 +5,7 @@
 #include <string_view>
 #include "gridsystem.h"
 
-int main() //currently does not compile. test_grid does though. Grid_Make and print_grid need work. Having trouble returning grid and printing it
+int main()
 {
     Vec2DChar grid (y_row, std::vector<char> (x_col, 'x'));
     print_grid(grid, y_row, x_col);
